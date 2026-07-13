a2de17e3d2f5b3a684e589fc95abeae9bb243a07
minhchn103 <m34ningless@gmail.com>
2026-07-13 15:58:10 +0700

    123

--- Program.cs
+++ Program.cs
@@ -28,6 +28,20 @@ Console.WriteLine($"ProjectId: {builder.Configuration["Firebase:ProjectId"]}");
 var credential =
 GoogleCredential.FromFile(credentialPath);
 
+try
+{
+    var token = await credential.UnderlyingCredential
+        .GetAccessTokenForRequestAsync();
+
+    Console.WriteLine("TOKEN OK");
+    Console.WriteLine(token.Substring(0, 20));
+}
+catch (Exception ex)
+{
+    Console.WriteLine("TOKEN ERROR");
+    Console.WriteLine(ex.ToString());
+}
+
 if (FirebaseApp.DefaultInstance == null)
 {
     FirebaseApp.Create(new AppOptions
